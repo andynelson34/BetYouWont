99bf657631cb66a4d2c9bcb2a1fe9031d6a125f7
Emily Shack <emshack92@gmail.com>
2015-04-25 16:21:59 -0500

    unpopulated table + logo

--- BetYouWon't/FirstViewController.m
+++ BetYouWon't/FirstViewController.m
@@ -12,9 +12,15 @@
 
 @interface FirstViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *newsFeedText;
+@property (weak, nonatomic) IBOutlet UITableView *newsFeed;
+@property (weak, nonatomic) IBOutlet UIScrollView *scroller;
+
 @end
 
 @implementation FirstViewController
+
+NSArray *tableData;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -32,8 +38,30 @@
             [tmpTabBarItem setEnabled:YES];
         }
         //Display news feed
+        tableData = [NSArray arrayWithObjects:@"Egg Benedict", @"Mushroom Risotto", @"Full Breakfast", @"Hamburger", @"Ham and Egg Sandwich", @"Creme Brelee", @"White Chocolate Donut", @"Starbucks Coffee", @"Vegetable Curry", @"Instant Noodle with Egg", @"Noodle with BBQ Pork", @"Japanese Noodle with Pork", @"Green Tea", @"Thai Shrimp Cake", @"Angry Birds Cake", @"Ham and Cheese Panini", nil];
+        
     }
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [tableData count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = @"newsFeed";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+    
+    cell.textLabel.text = [tableData objectAtIndex:indexPath.row];
+    cell.imageView.image = [UIImage imageNamed:@"BYWlogo2.png"];
+    return cell;
 }
 
 - (void)didReceiveMemoryWarning {
